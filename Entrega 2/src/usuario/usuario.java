7ee2dc84947b706f764617045f5ecda6d295eafc
Mateo Pardo <mateopardox18@gmail.com>
2026-04-03 16:24:00 -0500

    creacion de clase usuario

---
+++ Entrega 2/src/usuario/usuario.java
@@ -0,0 +1,12 @@
+package usuario;
+
+public abstract class usuario {
+	private int id;
+	private String login;
+	private String password;
+	private String nombre;
+	public usuario() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
