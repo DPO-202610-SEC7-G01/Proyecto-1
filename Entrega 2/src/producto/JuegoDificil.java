cf02e4a3421a4d8061125aff7519bec81dcad43c
ValentinaBustos2025 <av.bustos@uniandes.edu.co>
2026-04-03 21:16:25 -0500

    Juego Dificil

--- Entrega 2/src/producto/JuegoDificil.java
+++ Entrega 2/src/producto/JuegoDificil.java
@@ -1,5 +1,19 @@
 package producto;
 
 public class JuegoDificil extends Juego{
+	
+	
+	
+	public JuegoDificil(int id, int precio, String nombre, int anioPublicacion, String empresMatriz, int numJugadores,
+			String restriccionEdad, String categoria) {
+		super(id, precio, nombre, anioPublicacion, empresMatriz, numJugadores, restriccionEdad, categoria);
+	}
+		
+	//Métodos
 
+	@Override
+	public boolean requiereInstructor() {
+		return true;
+	}
+	
 }
