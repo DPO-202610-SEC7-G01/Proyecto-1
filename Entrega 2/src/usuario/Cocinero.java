dd5e95370139a01ecc9ed0486cec6810a7d6c874
ValentinaBustos2025 <av.bustos@uniandes.edu.co>
2026-04-04 21:47:17 -0500

    Comentarios y Revisada la clase de COCINERO

--- Entrega 2/src/usuario/Cocinero.java
+++ Entrega 2/src/usuario/Cocinero.java
@@ -6,24 +6,31 @@ import java.util.List;
 import producto.Bebida;
 import producto.Platillo;
 
-public class Cocinero extends empleado{
+public class Cocinero extends Empleado{
 	private List<Platillo> platillosConocidos;
 	private List<Bebida> bebidasConocidas;
+	
+	//Constructor
 	public Cocinero(int id, String login, String password, String nombre) {
 		super(id, login, password, nombre);
 		this.bebidasConocidas = new ArrayList<Bebida>();
 		this.platillosConocidos = new ArrayList<Platillo>();
 		
 	}
+	
+	//Getters y Setters
 	public List<Platillo> getPlatillosConocidos() {
 		return platillosConocidos;
 	}
 	public List<Bebida> getBebidasConocidas() {
 		return bebidasConocidas;
 	}
+	
+	//Métodos
 	public void aprenderPLatillo(Platillo platillo) {
 		this.platillosConocidos.add(platillo);
 	}
+	
 	public void aprenderBebida(Bebida bebida) {
 		this.bebidasConocidas.add(bebida);
 	}
